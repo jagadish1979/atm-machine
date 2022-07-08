e94280f40dbc4468fc9829f058b8c63b21b17187
jagadish1979 <42835631+jagadish1979@users.noreply.github.com>
2022-07-09 00:21:45 +0530

    Add files via upload

---
+++ src/main/java/com/zinkworks/atm/ennum/TransactionType.java
@@ -0,0 +1,12 @@
+package com.zinkworks.atm.ennum;
+
+/**
+ * TransactionType enum (DEPOSIT , WITHDRAW).
+ * 
+ * @author Jagadish Anala
+ **/
+public enum TransactionType {
+
+	DEPOSIT, WITHDRAW;
+
+}
